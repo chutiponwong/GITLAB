b93b54275d6e60e4bb53c6b70eaed888f666ea58
chutiponwong <chutiponwong@gmail.com>
2017-04-29 05:10:58 +0700

    Chutipon Modify bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,4 +1,4 @@
-//change by Chutipon
+//changed by Chutipon
 #include <stdio.h>
 #include <math.h>
 void main()
@@ -29,7 +29,7 @@ void main()
     }
     else if (strcmp(G, "B") ==0)
     {
-      printf("%s, B. Well, you can give blood 2: B, AB.\n", Name);
+      printf("%s, B. Well, you can give blood to: B, AB.\n", Name);
       printf("  You can receive blood from: B, O.\n");
     }
     else if (strcmp(G, "AB") == 0)
